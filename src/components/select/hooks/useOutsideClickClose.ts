030d3ee7002f444a5173eb4073f915c79f74e81e
s.volkova <s.volkova@owen.ru>
2024-07-30 23:43:12 +0300

    Третий коммит

--- src/components/select/hooks/useOutsideClickClose.ts
+++ src/components/select/hooks/useOutsideClickClose.ts
@@ -11,14 +11,13 @@ export const useOutsideClickClose = ({
 	isOpen,
 	rootRef,
 	onClose,
-	onChange,
 }: UseOutsideClickClose) => {
 	useEffect(() => {
+		if (!isOpen) return;
 		const handleClick = (event: MouseEvent) => {
 			const { target } = event;
 			if (target instanceof Node && !rootRef.current?.contains(target)) {
-				isOpen && onClose?.();
-				onChange?.(false);
+				onClose?.();
 			}
 		};
 
@@ -27,5 +26,5 @@ export const useOutsideClickClose = ({
 		return () => {
 			window.removeEventListener('mousedown', handleClick);
 		};
-	}, [onClose, onChange, isOpen]);
+	}, [isOpen, onClose, rootRef]);
 };
